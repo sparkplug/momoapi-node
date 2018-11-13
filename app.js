43b9d8e774ce306c0c5a46f9709605e2763c4d2b
nico@yoursole.com <nicoflojo.developer@gmail.com>
2018-11-13 13:26:39 +0300

    adds vanilla mock request

--- app.js
+++ app.js
@@ -49,3 +49,25 @@ request.post('https://flaviocopes.com/todos', {
   console.log(`statusCode: ${res.statusCode}`)
   console.log(body)
 })
+
+// Vanilla `http` get request (too verbose)
+function getLoginDetails(callback) {
+return http.get({
+        host: 'locatemap.in',
+        path: '/userDetail'
+    }, function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+// Data received, let us parse it using JSON!
+            var parsed = JSON.parse(body);
+            callback({
+                userDetail: parsed.name,
+                userId: parsed.Id
+            });
+        });
+    });
+},
